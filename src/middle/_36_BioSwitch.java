237f003ad7b8eb1032b6bb409cfccec3a1cc9de2
조명현 <@DESKTOP-4RRKBTM>
2018-09-29 17:41:05 +0900

    36

---
+++ src/middle/_36_BioSwitch.java
@@ -0,0 +1,23 @@
+package middle;
+
+public class _36_BioSwitch {
+
+	public static String textInfor(int index, double value)
+	{
+		String result="";
+		switch(index)
+		{
+		case 23:
+			result = "신체지수";
+		}
+		return result+"\t"+(value*100);
+	}
+	public static void main(String[] args) {
+		int index=23;
+		double value=0.97;
+		
+		String st=textInfor(index,value);
+		System.out.println(st);
+	}
+
+}
