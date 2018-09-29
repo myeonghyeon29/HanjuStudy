f3501f83e722066baa3cd97cf8efa897da3724a8
조명현 <@DESKTOP-4RRKBTM>
2018-09-29 15:26:06 +0900

    test2

--- src/middle/_28_BIoCalendar.java
+++ src/middle/_28_BIoCalendar.java
@@ -8,6 +8,6 @@ public class _28_BIoCalendar {
 		int days=1200;
 		double phyval=100*Math.sin((days%index)*2*Math.PI/index);
 		System.out.printf("나의 신체지수 %1$.2f입니다\n",phyval); //$앞에 숫자는 인자번호 f앞의 숫자는 소숫점 몇자리까지 표햔할지
-		System.out.println(phyval);  
+		System.out.println(phyval);  // dsadsad
 	}
 }
