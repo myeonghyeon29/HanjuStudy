3cdf4b4502fce5d2885f5acf5d67001caef29bbe
조명현 <@DESKTOP-4RRKBTM>
2018-09-30 15:07:32 +0900

    40

---
+++ src/middle/_40_Moneychange.java
@@ -0,0 +1,30 @@
+
+package middle;
+import java.util.Scanner;
+public class _40_Moneychange {
+	public static double tomoney(String ch) {
+		double exchange = 1000;
+		switch (ch) {
+		case "USD":
+			exchange *= 1.2;
+			break;
+		case "JPA":
+			exchange *= 1.1;
+			break;
+		case "CHA":
+			exchange *= 0.8;
+			break;
+		default:
+			System.out.println("없는나라!!");
+			break;
+		}
+		return exchange;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String ch = scanner.next();
+		System.out.printf("%.2f원\n",tomoney(ch));
+		scanner.close();
+	}
+}
