a48ca231cba0dca77f5cd8cb6bbcc0028bdc6b75
조명현 <@DESKTOP-4RRKBTM>
2018-09-30 16:15:02 +0900

    45

---
+++ src/middle/_45_ShowFor.java
@@ -0,0 +1,20 @@
+package middle;
+
+public class _45_ShowFor {
+	public static void Showfor(int n) {
+	int temp=n;
+	for(;temp!=1;) //조건문만 채우면 while처럼 사용가능
+	{
+		if(temp%2==1)
+			temp=temp*3+1;
+		else
+			temp/=2;
+	System.out.print("["+temp+"]");
+	}
+}
+
+	public static void main(String[] args) {
+		Showfor(122);
+	}
+
+}
